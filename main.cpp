fd9b60b0f983984cb4de917b7cba72bed353571d
ivlukin <Lukin2101>
2019-01-30 01:44:33 +0300

    just main to show how reader will work

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "Reader.h"
+
+//
+// Created by sorrow on 30.01.19.
+//
+int main() {
+    Reader r = Reader("/home/sorrow/North_rays/011014_00_N1_00.pnt");
+    r.readfile();
+    return 0;
+}
+
