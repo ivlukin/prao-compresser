d757245afffcf3af33c4ba040f64e2bd0b0da9d6
ivlukin <Lukin2101>
2019-02-01 01:40:13 +0300

    added reading of first 16 strings

--- Reader.cpp
+++ Reader.cpp
@@ -8,19 +8,31 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <vector>
 #include "Reader.h"
 
 Reader::Reader(std::string filename) {
     this->filename = std::move(filename);
 }
 
-std::string Reader::readfile() {
-    std::ifstream ifs(filename);
-    std::string line;
-    while (std::getline(ifs, line)) {
-        std::cout << line.c_str() << std::endl;
-    }
+std::vector<std::string> Reader::readfile() {
 
+    FILE *pFile;
+    pFile = fopen(&this->filename[0u], "rb");
+    std::vector<std::string> info;
+    for (int i = 0; i < 16; i++) {
+        char line[256];
+        fgets(line, 256, pFile);
+        std::string myLine(line);
+        info.push_back(myLine);
+        std::cout << myLine << std::endl;
+    }
+    float f;
+
+/*    while (fscanf(pFile, "%f", f) != EOF) {
+        std::cout << f << std::endl;
+    }*/
+    return info;
 }
 
 double Reader::calibrate(std::string values) {
