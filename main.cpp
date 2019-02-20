08a24256fece830f257209014af531a7072edbe8
ivlukin <Lukin2101>
2019-02-21 01:42:15 +0300

    some changes

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include "Reader.h"
 #include "Calibration/test.h"
 #include "Reader/testReader.h"
+#include "Processing/SignalProcessor.h"
 //
 // Created by sorrow on 30.01.19.
 //
 int main() {
-    Reader r = Reader("/home/sorrow/North_rays/011014_00_N1_00.pnt");
-    r.readfile();
+//    Reader r = Reader("/home/sorrow/North_rays/011014_00_N1_00.pnt");
+//    r.readfile();
 
-    testCalibrationStorage();
-    testReader();
+   // testCalibrationStorage();
+   // testReader();
+   SignalProcessor processor = SignalProcessor();
     return 0;
 }
 
