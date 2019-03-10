025bd90d383d2e12afe6e64e97b1c5c710041983
avdolgushev <avdolgushev@edu.hse.ru>
2019-03-10 18:59:50 +0300

    add comments to DataReader

--- Reader/DataReader.h
+++ Reader/DataReader.h
@@ -12,43 +12,58 @@
 #include <cstring>
 
 class DataReader{
-    bool is_header_parsed = false;
-    int count_read_points = 0, points_before_switch_calibration = 0;
-    int floats_per_point = 0, size_per_point = 0;
-    float points_per_chunk = 0, remainder = 0; // how much point the time chunk consists of; the remainder while reading integer of points_per_chunk
-    DataHeader dataHeader = {};
+    bool is_header_parsed = false; // true if header has been parsed
+    int count_read_points = 0, points_before_switch_calibration = 0; // how many points are already read from file; how many points remains with current calibration data
+    int floats_per_point = 0, size_per_point = 0; // how many floats during time resolution; the size of that floats
+    float points_per_chunk = 0, remainder = 0; // how many points the time chunk consists of; the remainder while reading integer of points_per_chunk
+    DataHeader dataHeader = {}; // header of the file
 
-    CalibrationDataStorage *calibration;
-    double *calibration_on_k = nullptr, *on_k_step = nullptr, *calibration_zr = nullptr, *zr_step = nullptr;
+    CalibrationDataStorage *calibration; // the storage that stores all related calibration data objects
+    double *calibration_on_k = nullptr, *on_k_step = nullptr, *calibration_zr = nullptr, *zr_step = nullptr; // data used during calibration
 
-    ifstream in;
+    ifstream in; // the stream from which reading is performed
     static constexpr int BUFFER_SIZE = 0x1000 * 33 * 7 * 3 * 4; // disk sector size + count of big channels + count of small channels + 48 modules + coef ~11MB
-    int buffer_pointer = BUFFER_SIZE;
-    char *buffer = nullptr;
+    int buffer_pointer = BUFFER_SIZE; // pointer for buffered read
+    char *buffer = nullptr; // buffer for buffered read
 
-    double timeChunk_duration_star = 0, timeChunk_duration_sun = 0;
+    double timeChunk_duration_star = 0, timeChunk_duration_sun = 0; // seconds of chunk to be read in star time and in sun time
 
 
     void readHeader();
     void realloc(double *& base, double const * from);
+
+    /// \breif sets appropriate calibration data regard to count_read_points
     void updateCalibrationData();
     void calibrateArrayPoints(float *point, int count);
     void calibrateArrayPointsDetailed(float *point, int size);
+
+    /// \breif read points recursive implementation
+    /// \param point destination
+    /// \param full_count count of points to read
+    /// \param offset offset in destination to which read local_count points
+    /// \param local_count count of points to read during current recursive call
     void readNextPointsInternal(float *point, int full_count, int offset, int local_count);
 public:
 
+    /// \param filepath path to file from which read
+    /// \param starSeconds_timeChunk_dur duration in star seconds of how many points will be read by call to readNextPoints
     explicit DataReader(string filepath, double starSeconds_timeChunk_dur);
     ~DataReader();
+
+    /// \breif set calibration signals
     void setCalibrationData(CalibrationDataStorage *calibrationData);
 
+    /// \breif get size of array that should be passed to read points for the specified in ctor time
     inline int getNeedBufferSize(){
         return floats_per_point * int(points_per_chunk + 1);
     }
-    /// used only for testing
+
+    /// \breif get count of floats in point regard to file time resolution
     inline int getPointSize() {
         return floats_per_point;
     }
 
+    /// \breif get date start in MJD format
     inline float get_MJD_begin(){
         return dataHeader.MJD_begin;
     }
@@ -56,20 +71,10 @@ public:
     inline bool eof() {
         return count_read_points >= dataHeader.npoints;
     }
-/*
-    /// ATTENTION: function depends on offset of data array in PointBase class. data array should be placed at 0 offset (or offset 4 if class has virtual methods == is_polymorphic<PointBase>::value)
-    inline void readNextPoint(PointBase *to) {
-        readNextPoints((float *) to);
-    }
 
-    inline void readNextPoint(PointSmall &to) {
-        readNextPoints((float *) to.data);
-    }
-
-    inline void readNextPoint(PointBig &to) {
-        readNextPoints((float *) to.data);
-    }
-    */
+    /// \brief read points regard to time chunk specified in ctor
+    /// \param to destination
+    /// \return count of points were read
     int readNextPoints(float *to);
     void readNextPoints(float *to, int count);
 };
