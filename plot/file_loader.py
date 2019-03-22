b0b3e2cfde76e1bcf0740f92eb38569106f4cb5f
Egor Solovev <e-solovev@yandex-team.ru>
2019-03-22 05:09:48 +0300

    fixed header reading

--- plot/file_loader.py
+++ plot/file_loader.py
@@ -17,8 +17,10 @@ def load(path):
     with open(path, 'rb') as fp:
         data = fp.read(1024)
         numpar = int(data.split('\r\n')[0].split('\t')[1])
+        logger.debug('numpar = %d', numpar)
 
-        header_length = sum([len(data.split('\r\n')[i]) + 2 for i in range(numpar + 1)])
+        header_length = sum([len(data.split('\r\n')[i]) + 2 for i in range(numpar)])
+        logger.debug('header_length = %d', header_length)
 
     time_started = time.time()
     as_float_array = array.array('f')
@@ -31,10 +33,10 @@ def load(path):
         header_raw = fp.read(header_length)
         header = dict(map(lambda x: (x.split('\t')[0], x.split('\t')[1]), filter(len, header_raw.split('\r\n'))))
         header['npoints'] = int(header['npoints'])
-        header['nmetrics'] = 9
+        header['nmetrics'] = 7
         header['nrays'] = 48
-        # header['nbands'] = int(header['nbands']) + 1
-        header['nbands'] = 33
+        header['nbands'] = int(header['nbands']) + 1
+        #header['nbands'] = 33
         logger.debug(header)
         fp.seek(header_length)
         as_float_array.fromfile(fp, file_length / 4)
